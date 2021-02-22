2b1e1c2c60bd0ca88b965328da95d38512a6ee1c
赵旋 <xuanzhao@88.com>
2021-02-22 18:16:18 +0800

    fix json

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,5 +1,28 @@
 <template>
-  <div id="loading"></div>
+  <!-- <q-circular-progress
+    show-value
+    indeterminate
+    font-size="10px"
+    class="q-ma-md"
+    size="200px"
+    :thickness="0.08"
+    color="dark-2"
+    track-color="grey-3"
+  >
+    <div id="loading"></div>
+  </q-circular-progress> -->
+  <q-inner-loading showing size="200px">
+    <div id="loading">
+      <!-- red-3,pink-3,purple-3 deep-purple-3 indigo-3 blue-3 light-blue-3 cyan-3 teal-3 green-3 light-green-3 lime-3 yellow-3 amber-3 orange-3 deep-orange-3 brown-3-->
+      <q-ajax-bar
+        ref="bar"
+        position="bottom"
+        :color="ajaxBarColor"
+        size="10px"
+        skip-hijack
+      />
+    </div>
+  </q-inner-loading>
 </template>
 
 <script>
@@ -7,17 +30,39 @@ import lottie from 'lottie-web';
 // import datajson from '../assets/lottie/loading.json';
 export default {
   name: 'Loading',
+  computed: {
+    ajaxBarColor() {
+      //
+      return [
+        'grey-3',
+        'red-3',
+        'pink-3',
+        'purple-3',
+        'deep-purple-3',
+        'indigo-3',
+        'blue-3',
+        'light-blue-3',
+        'cyan-3',
+        'teal-3',
+        'green-3',
+        'light-green-3',
+        'lime-3',
+        'light-green-3',
+        'yellow-3',
+        'amber-3',
+        'orange-3',
+        'deep-orange-3',
+        'brown-3',
+        'blue-grey-3',
+      ][Math.floor(0 + Math.random() * (20 - 0))];
+    },
+  },
   mounted() {
     this.lottieInit();
+    this.show();
   },
-  computed: {
-    loadingJson() {
-      const random = Math.random() * 10 > 5 ? '0' : '1';
-      const param = [{ name: 'covid' }, { name: 'loading' }];
-      console.log(['covid', 'loading'][Math.floor(Math.random() * 10) % 2]);
-      console.log(`${param[random]['name']}.json`);
-      return import(`../assets/lottie/${param[random]['name']}.json`);
-    },
+  destroyed() {
+    this.stop();
   },
   methods: {
     async lottieInit() {
@@ -28,6 +73,7 @@ export default {
           ]
         }.json`
       );
+      // const JsonData = await import(`../assets/lottie/10.json`);
       const icon = document.getElementById('loading');
       lottie.loadAnimation({
         container: icon, // 包含动画的dom元素
@@ -38,12 +84,12 @@ export default {
       });
     },
     show() {
-      this.showloading = true;
       lottie.play();
+      this.$refs.bar.start();
     },
-    hidden() {
-      this.showloading = false;
+    stop() {
       lottie.stop();
+      this.$refs.bar.stop();
     },
   },
 };
@@ -52,14 +98,14 @@ export default {
 <style lang="less">
 #loading {
   // background-color: #38c172;
-  width: 100%;
-  height: 100vh;
-  min-width: 31.25rem;
-  display: block;
-  overflow: hidden;
-  -webkit-transform: translateZ(0);
-  transform: translateZ(0);
-  text-align: center;
-  opacity: 1;
+  // width: 100%;
+  // height: 100vh;
+  // min-width: 31.25rem;
+  // display: block;
+  // overflow: hidden;
+  // -webkit-transform: translateZ(0);
+  // transform: translateZ(0);
+  // text-align: center;
+  // opacity: 1;
 }
 </style>
