ee24b7ac15851457561e91895a2ea31879f0a573
赵旋 <xuanzhao@88.com>
2021-03-01 14:25:56 +0800

    add home-category

---
+++ src/components/HomeCategory.vue
@@ -0,0 +1,51 @@
+<template>
+  <div :style="{ color: color }" class="home-category">
+    <p>{{ title }}</p>
+    <p>{{ count }}例</p>
+    <p>
+      <span class="tip">较昨日</span><span class="add">+{{ addcount }}</span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeCategory',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+    addcount: {
+      type: Number,
+      required: true,
+    },
+    count: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.home-category {
+  font-size: 14px;
+  font-weight: 800;
+  line-height: 16px;
+}
+.home-category p {
+  margin-bottom: 5px;
+}
+.home-category .add {
+  font-size: xx-small;
+}
+.home-category .tip {
+  color: gray;
+  font-size: xx-small;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,28 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <div>
+      <span class="allCountry">全国</span>
+      <span>截至(北京时间)</span>
+      <div class="categoryBox">
+        <HomeCategory :title="'确诊'" :color="'#e57471'" />
+        <HomeCategory :title="'疑似'" :color="'#dda451'" />
+        <HomeCategory :title="'重症'" :color="'#5d4037'" />
+        <HomeCategory :title="'死亡'" :color="'#919399'" />
+        <HomeCategory :title="'治愈'" :color="'#7ebe50'" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex';
-// import axios from 'axios';
+import HomeCategory from '@/components/HomeCategory';
 
 export default {
   name: 'Home',
+  components: {
+    HomeCategory,
+  },
   data() {
     return {};
   },
